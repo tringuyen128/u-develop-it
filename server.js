c1b06e63904f114f341846e06b2338e534c4e55b
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-03-09 14:18:06 -0800

    update candidates

--- server.js
+++ server.js
@@ -33,8 +33,37 @@ console.log(`server is running on ${PORT}`);
 // query method to call data of table
 db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
+}); 
+
+//get a single candidate
+/*
+ db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+}); 
+*/
+
+// Delete single candidate
+/* 
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+}) 
+*/
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
-
-
-
 
