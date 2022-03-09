ec69284f58872e114e266187fa5ea9ac67d96e82
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-03-09 09:42:13 -0800

    connect to database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const mysql = require('mysql2');
 const express = require('express');
+const { createConnection } = require('mysql2/typings/mysql');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +9,20 @@ const app = express();
 // express middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to Database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        // your mySQL username
+        user: 'root',
+        // your mySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('connected to election database.')
+);
+
 
 app.use((req, res) => {
     res.status(404).end();
